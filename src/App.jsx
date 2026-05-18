35fa86c458b7b21e62a7149cf640d95f53935297
filippozuppini <zuppini.filippo@gmail.com>
2026-05-18 12:17:54 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="heading-title">
+        <h1>Actors</h1>
+      </div>
+
+      <div class="row">
+        <div class="col-4">
+          <div class="card">
+            <div class="card-body">
+              <h3 class="card-title">Title</h3>
+              <p class="card-text">Text</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-4">
+          <div class="card">
+            <div class="card-body">
+              <h3 class="card-title">Title</h3>
+              <p class="card-text">Text</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+
+
+    </>
+  )
+}
+
+export default App
