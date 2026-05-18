30672d487d6efe0924d8a8330d978006478987ac
filippozuppini <zuppini.filippo@gmail.com>
2026-05-18 16:02:56 +0200

    milestone 1 compleata

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { useState, useEffect } from "react";
 
 function App() {
+
+  function fetchData(){
+    const url = 'https://lanciweb.github.io/demo/api/actors/';
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setActorsData(data)        
+      })
+  }
+
+  useEffect(fetchData, [])
+
+
+  const [actorsData, setActorsData] = useState([])
 
   return (
     <>
@@ -7,26 +23,28 @@ function App() {
         <h1>Actors</h1>
       </div>
 
-      <div class="row">
-        <div class="col-4">
-          <div class="card">
-            <div class="card-body">
-              <h3 class="card-title">Title</h3>
-              <p class="card-text">Text</p>
+      <div className="container">
+        <div className="row g-3">
+          <div className="col-12 col-sm-6 col-md-4">
+            <div className="card">
+
+
+              <img className="card-img-top" src="holder.js/100x180/" alt="Title" />
+              <div className="card-body">
+                <h4 className="card-title">Title</h4>
+                <p className="card-text">Text</p>
+                <p className="card-text">Text</p>
+                <p className="card-text">Text</p>
+                <p className="card-text">Text</p>
+                <p className="card-text">Text</p>
+
+              </div>
+
             </div>
-          </div>
-        </div>
-        <div class="col-4">
-          <div class="card">
-            <div class="card-body">
-              <h3 class="card-title">Title</h3>
-              <p class="card-text">Text</p>
-            </div>
+            
           </div>
         </div>
       </div>
-      
-
 
     </>
   )
